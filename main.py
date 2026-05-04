2cb354ab27c720f2567775d2905b50cb696d3869
lawmcl <mysticabsolut@gmail.com>
2026-05-04 12:42:20 +0800

    Add function to append student names to a list

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from pyscript import document
 
+def addStudent(event):
+    name = document.getElementById("name").value
+
+    if name == "":
+        return
+
+    li = document.createElement("li")
+    li.textContent = "Hi! I am " + name
+
+    document.getElementById("list").appendChild(li)
+
+    document.getElementById("name").value = ""
